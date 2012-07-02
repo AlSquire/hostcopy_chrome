7c9d48fb6041f3b21a5ebb3dcdca0f732ed80c27
Julien Ammous <schmurfy@gmail.com>
2012-07-02 13:53:46 +0200

    removed hostname field, auto adjust popup width

--- popup.js
+++ popup.js
@@ -3,16 +3,23 @@
 window.addEventListener("load", windowLoaded, false);
 function windowLoaded() {
   chrome.tabs.getSelected(null, function(tab) {
-    document.getElementById('current_uri').value = tab.url;
-    document.getElementById('current_host').value = getLocation(tab.url).hostname;
-    document.getElementById('current_host').focus();
-    document.getElementById('current_host').select();
+    var el = document.getElementById('current_uri');
+    el.value = tab.url;
+    el.focus();
+    el.setAttribute('size', el.value.length + 5);
+    
+    // document.getElementById('current_host').value = getLocation(tab.url).hostname;
+    // document.getElementById('current_host').select();
+    
+    // textRange = document.getElementById('current_uri').createTextRange();
+    // textRange.execCommand("RemoveFormat");
+     // document.execCommand('copy'); // require 'clipboardWrite' permissions
   });
 }
 // Parse the hostname from an uri using tricky javascript technology
 // From http://stackoverflow.com/questions/736513/how-do-i-parse-a-url-into-hostname-and-path-in-javascript
-var getLocation = function(href) {
-  var l = document.createElement("a");
-  l.href = href;
-  return l;
-};
+// var getLocation = function(href) {
+//   var l = document.createElement("a");
+//   l.href = href;
+//   return l;
+// };
